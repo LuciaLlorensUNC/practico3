98bc847cf969136a7a2b62ddcf5e57d82f90fd92
LuciaLlorens <luciallrns@gmail.com>
2023-09-12 23:13:48 -0300

    veo si me aparece gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Concha de la lora.
         </p>
         <a
           className="App-link"

c8787cee77797a5bdd1c739d1a0775be0d6a040f
LuciaLlorens <luciallrns@gmail.com>
2023-09-12 23:30:39 -0300

    repodrida estoy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Concha de la lora.
+          Edit <code>src/App.js</code> and save to reload. Concha de la lora. No se veeeeee.
         </p>
         <a
           className="App-link"

bbada725b01f3de178b5dc54edc8193a317162ee
LuciaLlorens <luciallrns@gmail.com>
2023-09-13 12:37:11 -0300

    corrijo obtencion de nombre y hago jugada PC

---
+++ src/componentes/JugadaPC.jsx
@@ -0,0 +1,20 @@
+import "../App.css";
+
+function JugadaPC() {
+    const obtenerJugadaComputadora = () => {
+        const lista = ["piedra", "papel", "tijera"];
+        const numero = Math.floor(Math.random()*3);
+        return lista[numero];
+    };
+
+    const jugadaComputadora= obtenerJugadaComputadora();
+
+    return (
+        <div>
+          <p>Jugada de la computadora: {jugadaComputadora}</p>
+          {/* No sé si quiero que esto se vea. Tal vez hacer aparecer solo al final*/}
+        </div>
+    );
+}
+
+export default JugadaPC;
--- src/componentes/ObtencionNombre.jsx
+++ src/componentes/ObtencionNombre.jsx
@@ -3,9 +3,15 @@ import '../App.css';
 
 function ObtencionNombre(props) {
     const [nombre, setNombreJugador] = useState("");
+    const [Saludo, setSaludar] = useState(false);
 
     const CambiarNombre = (e) => {
         setNombreJugador(e.target.value);
+        if (e.target.value !== '') {
+            setSaludar(true);
+        } else {
+            setSaludar(false);
+        }
     };
 
     return (
@@ -22,11 +28,12 @@ function ObtencionNombre(props) {
                     required=""
                 />
             </div>
-            <p>Hola, {nombre}!</p> {/*Este mensaje también tendría que estar oculto o podría poner un botón que confirme el nombre*/}
+
+            {Saludo && <p>Hola, {nombre || "humano"}!</p>} {/*Revisar si prefiero que el mensaje esté oculto hasta que empiece a escribir o si prefiero que aparezca como humano hasta que ingrese su nombre */}
             {/*Mensaje oculto; visible en el caso de que no se ingrese un nombre*/}
             <p id="mensajeCampoVacíoNombre">Ingresa un nombre para jugar!</p>
         </div>  
-    );
+    ); 
 }
 
 export default ObtencionNombre;
